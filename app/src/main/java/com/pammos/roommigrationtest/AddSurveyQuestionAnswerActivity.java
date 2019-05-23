e73a59c02d50352d90f96b4415a52d31ceb238cd
Diego Leandro Montaño López <lmontano@pammos.com>
2019-05-22 19:44:09 -0500

    Added creation activities for remaining entities.

---
+++ app/src/main/java/com/pammos/roommigrationtest/AddSurveyQuestionAnswerActivity.java
@@ -0,0 +1,13 @@
+package com.pammos.roommigrationtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSurveyQuestionAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_survey_question_answer);
+    }
+}
