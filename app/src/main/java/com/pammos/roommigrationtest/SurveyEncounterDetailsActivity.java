d6b2507aac41cb790b1258d3cf71a6456c4ed241
Diego Leandro Montaño López <lmontano@pammos.com>
2019-06-06 20:10:22 -0500

    Added details activities for remaining entities. BPM-HRM-SQ-SQCC done.
    Added and completed GUI layouts and controllers (only added) for details activities for Blood Pressure and Heartbeat Rate Measurements, Survey Question and Survey Question Coded Choice.
    Added GUI layouts and controllers for details activities for Survey Encounter and SurveyQuestion Answer.
    Registered new activities to manifest file.

---
+++ app/src/main/java/com/pammos/roommigrationtest/SurveyEncounterDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.pammos.roommigrationtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SurveyEncounterDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_encounter_details);
+    }
+}
